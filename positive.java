451096f23bef0ca43168e71cd56969a34ebd7d67
tanmaypanda47 <96779557+tanmaypanda47@users.noreply.github.com>
2023-03-29 21:38:29 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class positive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc. nextInt();
+        if(num>=00)
+        {
+            System.out.println("The Number Is Positive");
+        }
+        
+        else
+
+        {
+            System.out.println("The Number Is Negative");
+        }
+
+    }
+    
+}

2be5cb68e5515bc470c51dbcc2acf2ab944e37dd
tanmaypanda47 <96779557+tanmaypanda47@users.noreply.github.com>
2023-03-30 21:30:30 +0530

    Add files via upload

---
+++ sumint.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class sumint{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+   {
+    System.out.println("enter the numbers");
+   }
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+   int sum= (a+b);
+   {
+    System.out.println("the result is"  + sum);
+   }
+
+    }
+}
